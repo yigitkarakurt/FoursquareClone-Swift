375b4971b669c8fb5b3cd90b83ed4228d031f1a1
yigitkarakurt <yigitkarakurt35@gmail.com>
2022-11-28 15:37:22 +0300

    get datas from database

--- FoursquareClone/ViewController.swift
+++ FoursquareClone/ViewController.swift
@@ -12,7 +12,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        /*
+        
+        let parseObject = PFObject(className: "Fruits")
+        parseObject["name"] = "Banana"
+        parseObject["calories"] = 150
+        parseObject.saveInBackground { success, error in
+            if error != nil{
+                print(error?.localizedDescription)
+            }else{
+                print("uploaded")
+            }
+        }
+        
+        */
+        
+        let query = PFQuery(className: "Fruits")
+        query.whereKey("calories", greaterThan: 120)
+        query.findObjectsInBackground { objects, error in
+            if error != nil {
+                print(error?.localizedDescription)
+            }else{
+                print(objects)
+            }
+        }
+
+
     }
 
 
