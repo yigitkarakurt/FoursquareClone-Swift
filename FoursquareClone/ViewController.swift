497c298b64a4abd6ff7f403bece6197669ebfc78
yigitkarakurt <yigitkarakurt35@gmail.com>
2022-11-28 16:08:06 +0300

    sign in/up process

--- FoursquareClone/ViewController.swift
+++ FoursquareClone/ViewController.swift
@@ -8,10 +8,14 @@
 import UIKit
 import Parse
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController  {
+    
+    @IBOutlet weak var userNameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         /*
         
@@ -28,6 +32,8 @@ class ViewController: UIViewController {
         
         */
         
+        /*
+        
         let query = PFQuery(className: "Fruits")
         query.whereKey("calories", greaterThan: 120)
         query.findObjectsInBackground { objects, error in
@@ -37,9 +43,70 @@ class ViewController: UIViewController {
                 print(objects)
             }
         }
+        */
+        
+    }
+    
+    
+    @IBAction func signInClicked(_ sender: Any) {
+        
+        if userNameText.text != nil && passwordText != nil {
+            
+            PFUser.logInWithUsername(inBackground: userNameText.text!, password: passwordText.text!) { user, error in
+                
+                if error != nil {
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error!")
+                }else{
+                    //Segue
+                    print("welcome")
+                    print(user!.username!)
+                }
+                
+                
+            }
+            
+            
+        }else{
+            makeAlert(titleInput: "Error!", messageInput: "Username/Password?")
+        }
 
 
     }
+    
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        
+        if userNameText.text != nil && passwordText.text != nil {
+            let user = PFUser()
+            user.username = userNameText.text!
+            user.password = passwordText.text!
+            
+            user.signUpInBackground { success, error in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error!")
+                }else{
+                    //Segue
+                    print("OK!")
+                }
+            }
+            
+        }else{
+            makeAlert(titleInput: "Error", messageInput: "Username/Password?")
+            
+        }
+        
+    }
+    
+    func makeAlert(titleInput : String ,messageInput : String ){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+    }
+    
+    
+    
+
 
 
 }
