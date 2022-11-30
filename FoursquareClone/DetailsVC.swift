2455dad80968de915f47f60bda2b0474a783c26f
yigitkarakurt <yigitkarakurt35@gmail.com>
2022-11-30 18:15:01 +0300

    added details VC

---
+++ FoursquareClone/DetailsVC.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsVC.swift
+//  FoursquareClone
+//
+//  Created by Yiğit Karakurt on 30.11.2022.
+//
+
+import UIKit
+import MapKit
+
+class DetailsVC: UIViewController {
+    
+    @IBOutlet weak var detailsImageView: UIImageView!
+    @IBOutlet weak var detailsNameLabel: UILabel!
+    @IBOutlet weak var detailsTypeLabel: UILabel!
+    @IBOutlet weak var detailsCommentLabel: UILabel!
+    @IBOutlet weak var detailsMapView: MKMapView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
